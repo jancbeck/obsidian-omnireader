769f7e0d764d6440cc32abd70bef0bad0e6c4938
Jan Beck <jancbeck@users.noreply.github.com>
2024-11-01 19:10:41 +0100

    use codemirror extension

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import {
 } from "obsidian";
 import { AnnotateModal } from "./AnnotateModal";
 import allColorNames from "./colors";
+import { annotationExtension } from "./annotationExtension";
 
 const DEFAULT_SETTINGS = {
 	expandSelection: true,
@@ -18,57 +19,48 @@ type OmnireaderSettings = typeof DEFAULT_SETTINGS;
 
 export default class OmnireaderPlugin extends Plugin {
 	settings: OmnireaderSettings;
-	modalOpen = false;
+	isModalOpen = false;
+	isAnnotateModeOn = false;
+	statusBarItemEl: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
+		// Icon in the left ribbon to toggle annotation mode
 		const ribbonIconEl = this.addRibbonIcon(
 			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
+			`${this.isAnnotateModeOn ? "Disable" : "Enable"} Annotation Mode`,
+			() => {
+				this.toggleAnnotateMode();
 			}
 		);
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+		this.statusBarItemEl = this.addStatusBarItem();
+		this.statusBarItemEl.setText(`Annotate Mode: ${this.isAnnotateModeOn}`);
+		this.statusBarItemEl.addEventListener("click", () =>
+			this.toggleAnnotateMode()
+		);
 
-		// This adds a simple command that can be triggered anywhere
+		// Add the annotation extension to CodeMirror
+		this.registerEditorExtension(annotationExtension());
+
+		// Add a command to create annotations
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				// new AnnotateModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
+			id: "create-annotation",
+			name: "Create Highlight",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView =
 					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				const selectedText = markdownView?.editor.getSelection();
+				if (markdownView && selectedText) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						// new AnnotateModal(this.app).open();
+						this.createHighlightCommand(markdownView.editor);
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -80,176 +72,68 @@ export default class OmnireaderPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new OmnireaderSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// highlight selected text when in annotate mode
 		this.registerDomEvent(document, "mouseup", async (e: MouseEvent) => {
 			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 
 			const editor = view?.editor;
-			let selection = editor?.getSelection();
+			const selectionText = editor?.getSelection();
 
-			// if (view?.getMode() == "preview") {
-			// }
-			const target = e.target as HTMLElement | null;
+			if (!editor || !selectionText) return;
 
-			// when existing highlight is clicked open modal with existing content & color
-			if (target?.hasClass("cm-highlight") && editor) {
-				// wait for the cursor to be placed inside the highlight
-				await new Promise((resolve) => setTimeout(resolve, 10));
+			// require modifier key when not in annotate mode
+			if (!e.metaKey && !this.isAnnotateModeOn) return;
 
-				// move cursor to end of highlight
-				moveCursorToNextString(editor, "==");
-
-				let footnoteText = "";
-				let higlightColor = "";
-
-				// check if inline footnote already exists
-				const footnote = matchInlineFootnote(editor);
-				// accept all color names as valid color options
-				const colorOptions = allColorNames.map((color) =>
-					color.name.toLowerCase()
-				);
-
-				// select footnote content
-				if (footnote) {
-					footnoteText = footnote.content;
-					editor.setSelection(footnote.from, footnote.to);
-					const matchedColors = Array.from(
-						footnoteText.matchAll(/@(\w+)/g)
-					);
-
-					if (matchedColors.length > 0) {
-						// find first matched color that is in colorOptions
-						higlightColor =
-							Array.from(matchedColors).find(([_, color]) =>
-								colorOptions.includes(color)
-							)?.[1] || "";
-
-						footnoteText = footnoteText.replace(
-							new RegExp(` ?@${higlightColor}`, "g"),
-							""
-						);
-					}
-				}
-				openAnnotateModal(footnoteText, higlightColor);
-			}
-
-			if (!selection || !editor?.hasFocus()) {
-				return;
-			}
-
-			if (this.settings.expandSelection) {
-				expandSelectionBoundary(editor);
-			}
-			selection = editor?.getSelection();
-			// apply built-in highlight command which handles edge cases
-			(
-				this.app as App & {
-					commands: { executeCommandById: (cmd: string) => void };
-				}
-			).commands.executeCommandById("editor:toggle-highlight");
-
-			// move cursor to end of highlight
-			moveCursorToNextString(editor, "==");
-
-			openAnnotateModal();
+			this.createHighlightCommand(editor);
 		});
 
-		function matchInlineFootnote(editor: Editor) {
-			const cursor = editor.getCursor();
-			const line = editor.getLine(cursor.line);
-			const textAfterCursor = line.substring(cursor.ch);
+		// open modal when existing highlight is clicked
+		// this.registerDomEvent(document, "mouseup", async (e: MouseEvent) => {
+		// 	const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 	const editor = view?.editor;
+		// 	const target = e.target as HTMLElement | null;
 
-			// Match pattern ^[...] with any content inside brackets
-			const footnoteMatch = textAfterCursor
-				.trimStart()
-				.match(/^\^\[((?:[^[\]]|\[(?:[^[\]]|\[[^[\]]*\])*\])*)\]/);
+		// 	// when existing highlight is clicked open modal with existing content & color
+		// 	if (target?.hasClass("cm-highlight") && editor) {
+		// 		// wait for the cursor to be placed inside the highlight
+		// 		await new Promise((resolve) => setTimeout(resolve, 10));
 
-			if (footnoteMatch) {
-				const whitespaceBeforeFootnote =
-					textAfterCursor.length - textAfterCursor.trimStart().length;
-				const matchStart = cursor.ch + whitespaceBeforeFootnote;
-				const matchEnd = matchStart + footnoteMatch[0].length;
+		// 		let footnoteText = "";
+		// 		let higlightColor = "";
 
-				return {
-					from: { line: cursor.line, ch: matchStart },
-					to: { line: cursor.line, ch: matchEnd },
-					content: footnoteMatch[1], // Group 1 contains just the content inside brackets
-				};
-			}
+		// 		// check if inline footnote already exists
+		// 		const footnote = this.matchInlineFootnote(editor);
 
-			return null;
-		}
+		// 		// select footnote content
+		// 		if (footnote) {
+		// 			footnoteText = footnote.content;
+		// 			editor.setSelection(footnote.from, footnote.to);
+		// 			const matchedColors = Array.from(
+		// 				footnoteText.matchAll(/@(\w+)/g)
+		// 			);
 
-		function moveCursorToNextString(editor: Editor, searchString: string) {
-			const cursor = editor.getCursor();
-			const lastLine = editor.lastLine();
-			const currentLine = cursor.line;
-			const currentCh = cursor.ch;
+		// 			if (matchedColors.length > 0) {
+		// 				// find first matched color that matches any possible color name
+		// 				higlightColor =
+		// 					Array.from(matchedColors).find(([_, color]) =>
+		// 						allColorNames
+		// 							.map((color) => color.name.toLowerCase())
+		// 							.includes(color)
+		// 					)?.[1] || "";
 
-			// Search through current line first
-			const line = editor.getLine(currentLine);
-			const indexInCurrentLine = line.indexOf(searchString, currentCh);
-
-			if (indexInCurrentLine !== -1) {
-				// Found == in current line
-				editor.setCursor({
-					line: currentLine,
-					ch: indexInCurrentLine + 2,
-				});
-				return true;
-			}
-
-			// Search through subsequent lines
-			for (
-				let searchLine = currentLine + 1;
-				searchLine <= lastLine;
-				searchLine++
-			) {
-				const line = editor.getLine(searchLine);
-				const index = line.indexOf(searchString);
-
-				if (index !== -1) {
-					// Found == in this line
-					editor.setCursor({ line: searchLine, ch: index + 2 });
-					return true;
-				}
-			}
-
-			// No == found
-			return false;
-		}
-
-		function expandSelectionBoundary(editor: Editor) {
-			const from = editor.getCursor("from");
-			const to = editor.getCursor("to");
-			const lineFrom = editor.getLine(from.line);
-			const lineTo = editor.getLine(to.line);
-			let start = from.ch;
-			let end = to.ch;
-			while (
-				start > 0 &&
-				lineFrom[start - 1].match(/\w/) &&
-				lineFrom.substring(start - 2, start) !== "=="
-			) {
-				start--;
-			}
-			while (
-				end < lineTo.length &&
-				lineTo[end].match(/\w/) &&
-				lineTo.substring(end, end + 2) !== "=="
-			) {
-				end++;
-			}
-			editor.setSelection(
-				{ line: from.line, ch: start },
-				{ line: to.line, ch: end }
-			);
-		}
+		// 				footnoteText = footnoteText.replace(
+		// 					new RegExp(` ?@${higlightColor}`, "g"),
+		// 					""
+		// 				);
+		// 			}
+		// 		}
+		// 		openAnnotateModal(footnoteText, higlightColor);
+		// 	}
+		// });
 
 		const openAnnotateModal = (annotation = "", higlightColor = "") => {
-			if (!this.modalOpen) {
-				this.modalOpen = true;
+			if (!this.isModalOpen) {
+				this.isModalOpen = true;
 				const modal = new AnnotateModal(
 					this.app,
 					(result) => {
@@ -278,7 +162,7 @@ export default class OmnireaderPlugin extends Plugin {
 				);
 				modal.open();
 				modal.onClose = async () => {
-					this.modalOpen = false;
+					this.isModalOpen = false;
 
 					const editor =
 						this.app.workspace.getActiveViewOfType(
@@ -298,6 +182,12 @@ export default class OmnireaderPlugin extends Plugin {
 		};
 
 		this.registerMarkdownPostProcessor((element, ctx) => {
+			const marks = element.findAll("mark");
+			for (const mark of marks) {
+				console.log(mark.nextElementSibling, ctx.getSectionInfo(mark));
+			}
+
+			return;
 			const references = element.findAll("mark + sup.footnote-ref");
 			// turn higlights into footnote references
 			for (const ref of references) {
@@ -350,6 +240,100 @@ export default class OmnireaderPlugin extends Plugin {
 				footnote.innerText = footnote.innerText.replace(/@(\w+)/, "");
 			}
 		});
+	}
+
+	toggleAnnotateMode() {
+		this.isAnnotateModeOn = !this.isAnnotateModeOn;
+		this.statusBarItemEl.setText(`Annotate Mode: ${this.isAnnotateModeOn}`);
+		new Notice(
+			`Annotation Mode ${this.isAnnotateModeOn ? "Enabled" : "Disabled"}`
+		);
+	}
+	createHighlightCommand(editor: Editor) {
+		let selectedText = editor.getSelection();
+
+		if (!selectedText) {
+			new Notice(`No text selected`);
+			return false;
+		}
+
+		const sameLine =
+			editor.getCursor("from").line === editor.getCursor("to").line;
+
+		if (!sameLine) {
+			new Notice(`Only same line highlights are supported. Sorry!`);
+			return false;
+		}
+
+		const selectionContainsHighlight = selectedText.includes("==");
+
+		if (selectionContainsHighlight) {
+			new Notice(`Selection already contains a highlight`);
+			return false;
+		}
+
+		if (this.settings.expandSelection) {
+			selectedText = this.expandSelectionBoundary(editor);
+		}
+
+		const annotationText = `==${selectedText}==`;
+
+		editor.replaceSelection(annotationText);
+		editor.blur();
+	}
+
+	expandSelectionBoundary(editor: Editor) {
+		const from = editor.getCursor("from");
+		const to = editor.getCursor("to");
+		const lineFrom = editor.getLine(from.line);
+		const lineTo = editor.getLine(to.line);
+		let start = from.ch;
+		let end = to.ch;
+		while (
+			start > 0 &&
+			lineFrom[start - 1].match(/\w/) &&
+			lineFrom.substring(start - 2, start) !== "=="
+		) {
+			start--;
+		}
+		while (
+			end < lineTo.length &&
+			lineTo[end].match(/\w/) &&
+			lineTo.substring(end, end + 2) !== "=="
+		) {
+			end++;
+		}
+		editor.setSelection(
+			{ line: from.line, ch: start },
+			{ line: to.line, ch: end }
+		);
+		return editor.getSelection();
+	}
+
+	matchInlineFootnote(editor: Editor) {
+		const cursor = editor.getCursor();
+		const line = editor.getLine(cursor.line);
+		const textAfterCursor = line.substring(cursor.ch);
+
+		// Match pattern ^[...] with any content inside brackets
+		const footnoteMatch = textAfterCursor
+			.trimStart()
+			.match(/^\^\[((?:[^[\]]|\[(?:[^[\]]|\[[^[\]]*\])*\])*)\]/);
+
+		if (footnoteMatch) {
+			const whitespaceBeforeFootnote =
+				textAfterCursor.length - textAfterCursor.trimStart().length;
+			const matchStart = cursor.ch + whitespaceBeforeFootnote;
+			const matchEnd = matchStart + footnoteMatch[0].length;
+
+			return {
+				from: { line: cursor.line, ch: matchStart },
+				to: { line: cursor.line, ch: matchEnd },
+				content: footnoteMatch[1], // Group 1 contains just the content inside brackets
+			};
+		}
+
+		return null;
 	}
 
 	onunload() {}
